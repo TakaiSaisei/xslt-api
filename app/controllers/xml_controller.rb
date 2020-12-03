c520e86755d7e4750c1b37722b07abb5c2b7c747
TakaiSaisei <takaisaisei@gmail.com>
2020-12-04 01:27:45 +0300

    Final tests

--- app/controllers/xml_controller.rb
+++ app/controllers/xml_controller.rb
@@ -3,22 +3,22 @@
 class XmlController < ApplicationController
   before_action :parse_params, only: :index
   def index
-    limit = params[:limit].to_i
-    @result = automorphs(limit).map { |element| [element, element**2] }
+    @result = automorphs(@limit).map { |element| { x: element, xsqr: element**2 } }
     respond_to do |format|
+      format.html { render }
       format.xml { render xml: @result.to_xml }
       format.rss { render xml: @result.to_xml }
     end
   end
 
   def parse_params
-    @limit = params[:limit]
+    @limit = params[:limit].to_i
   end
 
   private
 
   def automorphs(num)
-    array = (1..num).select { |element| [1, 5, 6].member?(element % 10) }
+    array = (1..@limit).select { |element| [1, 5, 6].member?(element % 10) }
     array.delete_if { |element| !(element**2).to_s.end_with?(element.to_s) }
   end
 end
--- test/controllers/xml_controller_test.rb
+++ test/controllers/xml_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class XmlControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'check xml format' do
+    get '/?limit=1&format=xml'
+    assert_response :success
+    assert_includes @response.headers['Content-Type'], 'application/xml'
+  end
+
+  test 'check rss format' do
+    get '/?limit=1&format=rss'
+    assert_response :success
+    assert_includes @response.headers['Content-Type'], 'application/rss'
+  end
+
 end
